67d45d64f4ae7507deae5384a1e23bf7c51217af
hxsm <romain.alexandre5@orange.fr>
2023-06-15 15:15:55 +0200

    ajout affichage des catégories + modification du header

---
+++ admin/src/action/getCategorieAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MiniPress\app\action;
+
+use MiniPress\app\service\CategorieService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+class getCategorieAction
+{
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $categorieService = new CategorieService();
+        $categories = $categorieService->getCategories();
+
+        $view = Twig::fromRequest($request);
+
+        return $view->render($response, 'Categories.twig', ['categories' => $categories]);
+    }
+}

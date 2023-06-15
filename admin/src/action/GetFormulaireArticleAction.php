6a7dd001bb19dad6a79d0ca2bc122dd8cc70af92
Sionrv <totoledose@outlook.fr>
2023-06-15 10:38:24 +0200

    getfomulaireact

---
+++ admin/src/action/GetFormulaireArticleAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MiniPress\app\action;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
+
+class GetFormulaireArticleAction{
+
+    public function __invoke(Request $rq, Response $rs, $args):Response{
+
+        $twig = Twig::fromRequest($rq);
+        return $twig->render($rs, 'FormulaireArticle.twig');
+    }
+}

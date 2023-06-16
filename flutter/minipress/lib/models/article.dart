b0c01fa6de3a04394ddfc7ddd02f8a4576bc8fe6
hxsm <romain.alexandre5@orange.fr>
2023-06-16 10:51:35 +0200

    création classe article dans le modèle

---
+++ flutter/minipress/lib/models/article.dart
@@ -0,0 +1,22 @@
+class Article {
+  final String id;
+  final String titre;
+  final String auteur;
+  final DateTime createdAt;
+
+  Article({
+    required this.id,
+    required this.titre,
+    required this.auteur,
+    required this.createdAt,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      id: json['id'],
+      titre: json['titre'],
+      auteur: json['auteur'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+}

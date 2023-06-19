edf0c63ef7fdd83ba2018a6a30d2af375de11f46
hxsm <romain.alexandre5@orange.fr>
2023-06-19 14:49:27 +0200

    fonctionnalité 1 terminée

--- flutter/minipress/lib/providers/api_provider.dart
+++ flutter/minipress/lib/providers/api_provider.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ApiProvider {
-  static const baseUrl = 'http://docketu.iutnc.univ-lorraine.fr:60603';
+  static const baseUrl = 'http://docketu.iutnc.univ-lorraine.fr:17012';
 
   static Future<http.Response> get(String endpoint) async {
     final url = Uri.parse('$baseUrl$endpoint');

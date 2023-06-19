37512ebf9ee942e1a5c8d5a620ecc7316c2228f9
lucarafe <luc.bonin0903@gmail.com>
2023-06-19 15:43:29 +0200

    modif API

--- api/src/action/getArticlesByCategIDAction.php
+++ api/src/action/getArticlesByCategIDAction.php
@@ -41,6 +41,7 @@ class getArticlesByCategIDAction
         $response->getBody()->write(json_encode($data));
         return
             $response->withHeader('Content-Type','application/json')
+                ->withHeader('Access-Control-Allow-Origin','*')
                 ->withStatus(200);
     }
 

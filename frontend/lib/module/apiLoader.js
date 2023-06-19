702dbea0d1b46a4292d73997e22aa0f6418fa9b3
lucarafe <luc.bonin0903@gmail.com>
2023-06-19 15:43:50 +0200

    Debut frontend

--- frontend/lib/module/apiLoader.js
+++ frontend/lib/module/apiLoader.js
@@ -1,29 +1,7 @@
 import {config} from '../conf/config.js';
 
-export function loadArticles(api = '/api/articles'){
-    return fetch(config.entryPoint + api)
-        .then(response => {
-            if (response.ok) return response.json();
-            else return Promise.reject(new Error(response.statusText))
-        })
-        .catch(error => {
-            console.log('error : '+error);
-        });
-}
-
-export function loadCategories(){
-    return fetch(config.entryPoint + '/api/categories')
-        .then(response => {
-            if (response.ok) return response.json();
-            else return Promise.reject(new Error(response.statusText))
-        })
-        .catch(error => {
-            console.log('error : '+error);
-        });
-}
-
-export function loadData(url){
-    return fetch(config.entryPoint + url)
+export function loadArticles(){
+    return fetch(config.entryPoint + '/api/articles')
         .then(response => {
             if (response.ok) return response.json();
             else return Promise.reject(new Error(response.statusText))

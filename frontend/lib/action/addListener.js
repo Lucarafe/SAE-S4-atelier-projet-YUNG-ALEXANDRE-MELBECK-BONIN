f14ac7b4ededd8ad27d40a5fa8bc44da6881655a
lucarafe <luc.bonin0903@gmail.com>
2023-06-20 14:46:36 +0200

    ajout trie/ et filtrage frontend

--- frontend/lib/action/addListener.js
+++ frontend/lib/action/addListener.js
@@ -1,5 +1,6 @@
-import {loadData} from "../module/apiLoader.js";
-import {display_article, display_articles} from "../display/display.js";
+import {loadArticles, loadCategories, loadData} from "../module/apiLoader.js";
+import {display_article, display_articles, displayArticles} from "../display/display.js";
+import {config} from '../conf/config.js';
 
 export function articleAction(div){
     div.addEventListener('click',  function (){
@@ -17,10 +18,65 @@ export function categorieAction(li){
     })
 }
 
+export function categoriesAction(li){
+    li.addEventListener('click',  function (){
+        loadArticles().then(article => {
+            display_articles(article.articles);
+        });
+    })
+}
+
 export function userAction(p){
     p.addEventListener('click',  function (){
         loadData(this.getAttribute('url')).then(art =>{
             display_articles(art.articles);
         });
     })
+}
+
+export function ButtonTrieAction(boutonTri){
+    boutonTri.addEventListener('click', function (){
+        const ordre = this.dataset.ordre;
+
+        if (ordre === config.trieAsc) {
+            trierArticlesAscendant(this);
+        } else {
+            trierArticlesDescendant(this);
+        }
+    })
+}
+
+function trierArticlesAscendant(boutonTri) {
+    loadArticles(boutonTri.dataset.ordre).then(article =>{
+        display_articles(article.articles);
+    })
+
+    boutonTri.textContent = 'Trier par ordre décroissant';
+    boutonTri.dataset.ordre = config.trieDesc;
+}
+
+
+function trierArticlesDescendant(boutonTri) {
+    loadArticles(boutonTri.dataset.ordre).then(article =>{
+        display_articles(article.articles);
+    })
+
+    boutonTri.textContent = 'Trier par ordre croissant';
+    boutonTri.dataset.ordre = config.trieAsc;
+}
+
+export function searchBarAction(searchBar, articles){
+    searchBar.addEventListener('keyup', (e) => {
+        const searchString = e.target.value.toLowerCase();
+
+        const filteredArticles = articles.filter((art) => {
+            return (
+                art.article.titre.toLowerCase().includes(searchString) ||
+                art.article.resume.toLowerCase().includes(searchString)
+            );
+        });
+        const articleListe = document.getElementById('liste');
+        articleListe.textContent = "";
+        displayArticles(filteredArticles, articleListe);
+    });
 }
--- frontend/lib/conf/config.js
+++ frontend/lib/conf/config.js
@@ -1,3 +1,5 @@
 export let config = {
     entryPoint: "http://docketu.iutnc.univ-lorraine.fr:60607",
+    trieAsc: '/api/articles?sort=date-asc',
+    trieDesc: '/api/articles?sort=date-desc'
 };

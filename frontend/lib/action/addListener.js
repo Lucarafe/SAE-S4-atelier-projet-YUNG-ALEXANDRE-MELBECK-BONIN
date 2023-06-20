54a97bdef782f6537126baf011ac89d6083aff71
lucarafe <luc.bonin0903@gmail.com>
2023-06-20 10:51:21 +0200

    fonction 1 à 5 frontEND

--- frontend/lib/action/addListener.js
+++ frontend/lib/action/addListener.js
@@ -1,61 +1,26 @@
-import {loadArticles, loadCategories, loadData} from "../module/apiLoader.js";
-import {display_article, display_articles, displayArticles} from "../display/display.js";
-import {config} from '../conf/config.js';
+import {loadData} from "../module/apiLoader.js";
+import {display_article, display_articles} from "../display/display.js";
 
-export function articleAction(div) {
-    div.addEventListener('click',  function () {
-        loadData(this.getAttribute('url'))
-            .then(art => display_article(art));
-    });
-}
-
-export function categorieAction(li) {
-    li.addEventListener('click',  function () {
-        loadData(this.getAttribute('url'))
-            .then(categ => display_articles(categ.articles));
-    });
-}
-
-export function categoriesAction(li) {
-    li.addEventListener('click',  function () {
-        loadArticles()
-            .then(article => display_articles(article.articles));
-    });
-}
-
-export function userAction(p) {
-    p.addEventListener('click',  function () {
-        loadData(this.getAttribute('url'))
-            .then(art => display_articles(art.articles));
-    });
-}
-
-export function ButtonTrieAction(boutonTri) {
-    boutonTri.addEventListener('click', function () {
-        const ordre = this.dataset.ordre;
-        const newOrdre = (ordre === config.trieAsc) ? config.trieDesc : config.trieAsc;
-
-        loadArticles(newOrdre)
-            .then(article => display_articles(article.articles));
-
-        boutonTri.textContent = (newOrdre === config.trieAsc) ? 'Trier par ordre décroissant' : 'Trier par ordre croissant';
-        boutonTri.dataset.ordre = newOrdre;
-    });
-}
-
-export function searchBarAction(searchBar, articles) {
-    searchBar.addEventListener('keyup', (e) => {
-        const searchString = e.target.value.toLowerCase();
-
-        const filteredArticles = articles.filter((art) => {
-            return (
-                art.article.titre.toLowerCase().includes(searchString) ||
-                art.article.resume.toLowerCase().includes(searchString)
-            );
+export function articleAction(div){
+    div.addEventListener('click',  function (){
+        loadData(this.getAttribute('url')).then(art =>{
+            display_article(art);
         });
+    })
+}
 
-        const articleListe = document.getElementById('liste');
-        articleListe.textContent = "";
-        displayArticles(filteredArticles, articleListe);
-    });
+export function categorieAction(li){
+    li.addEventListener('click',  function (){
+        loadData(this.getAttribute('url')).then(categ =>{
+            display_articles(categ.articles);
+        });
+    })
+}
+
+export function userAction(p){
+    p.addEventListener('click',  function (){
+        loadData(this.getAttribute('url')).then(art =>{
+            display_articles(art.articles);
+        });
+    })
 }
--- frontend/lib/module/apiLoader.js
+++ frontend/lib/module/apiLoader.js
@@ -10,3 +10,25 @@ export function loadArticles(){
             console.log('error : '+error);
         });
 }
+
+export function loadCategories(){
+    return fetch(config.entryPoint + '/api/categories')
+        .then(response => {
+            if (response.ok) return response.json();
+            else return Promise.reject(new Error(response.statusText))
+        })
+        .catch(error => {
+            console.log('error : '+error);
+        });
+}
+
+export function loadData(url){
+    return fetch(config.entryPoint + url)
+        .then(response => {
+            if (response.ok) return response.json();
+            else return Promise.reject(new Error(response.statusText))
+        })
+        .catch(error => {
+            console.log('error : '+error);
+        });
+}

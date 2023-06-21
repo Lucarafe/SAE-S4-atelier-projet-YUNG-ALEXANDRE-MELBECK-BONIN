eec00c7046bd9dfc8a7e5aebc3a25cd483f79090
hxsm <romain.alexandre5@orange.fr>
2023-06-21 09:44:45 +0200

    correction articles pages et auteur_articles_page

--- flutter/minipress/lib/screens/auteur_articles_page.dart
+++ flutter/minipress/lib/screens/auteur_articles_page.dart
@@ -1,33 +1,69 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import '../models/article.dart';
+import '../providers/api_provider.dart';
 
-class AuthorArticlesPage extends StatelessWidget {
-  final String author;
-  final List<Article> articles;
+class AuthorArticlesPage extends StatefulWidget {
+  final Article article;
 
   const AuthorArticlesPage({
-    required this.author,
-    required this.articles,
+    required this.article,
   });
+
+  @override
+  _AuthorArticlesPageState createState() => _AuthorArticlesPageState();
+}
+
+class _AuthorArticlesPageState extends State<AuthorArticlesPage> {
+  List<Article> articles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAuthorArticles();
+  }
+
+  void fetchAuthorArticles() async {
+    final response = await ApiProvider.get(widget.article.hrefAuteur);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      final List<Article> fetchedArticles = responseData.map((data) {
+        final String id = data['id'].toString();
+        final String titre = data['titre'].toString();
+        final String resume = data['resume'].toString();
+        final String contenu = data['contenu'].toString();
+        final String auteur = data['auteur'].toString();
+        final DateTime createdAt = DateTime.parse(data['created_at']);
+        return Article(
+          id: id,
+          titre: titre,
+          resume: resume,
+          contenu: contenu,
+          auteur: auteur,
+          createdAt: createdAt,
+          href: widget.article.href,
+          hrefAuteur: widget.article.hrefAuteur,
+        );
+      }).toList();
+
+      setState(() {
+        articles = fetchedArticles;
+      });
+    } else {
+      throw Exception('Failed to fetch author articles');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Auteur des articles'),
+        title: Text('Articles by ${widget.article.auteur}'),
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Les articles par $author',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
           Expanded(
             child: ListView.builder(
               itemCount: articles.length,
@@ -36,7 +72,7 @@ class AuthorArticlesPage extends StatelessWidget {
                 return ListTile(
                   title: Text(article.titre),
                   subtitle: Text(
-                    'Created on: ${article.createdAt.year}-${article.createdAt.month}-${article.createdAt.day}',
+                    'Créé le: ${article.createdAt.year}-${article.createdAt.month}-${article.createdAt.day}',
                   ),
                   onTap: () {
                     // Handle article tap

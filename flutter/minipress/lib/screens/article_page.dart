eb3b446893a27d90c93dbd8585b4a4687d0a4d9c
lucarafe <luc.bonin0903@gmail.com>
2023-06-21 10:40:22 +0200

    correction variable contenu = null flutter

--- flutter/minipress/lib/screens/article_page.dart
+++ flutter/minipress/lib/screens/article_page.dart
@@ -7,7 +7,7 @@ import '../providers/api_provider.dart';
 import 'auteur_articles_page.dart';
 
 class ArticlePage extends StatefulWidget {
-  late final Article article;
+  Article article;
 
   ArticlePage({required this.article});
 
@@ -18,6 +18,7 @@ class ArticlePage extends StatefulWidget {
 class _ArticlePageState extends State<ArticlePage> {
   String? articleContent;
   String? articleResume;
+  Article? fetchedArticle;
 
   @override
   void initState() {
@@ -30,27 +31,26 @@ class _ArticlePageState extends State<ArticlePage> {
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      final String contentArticle = data['article']['contenu'];
+
+      final String contenuArticle = data['article']['contenu'];
       final String resumeArticle = data['article']['resume'];
       final String id = data['article']['id'].toString();
       final String titre = data['article']['titre'];
       final String auteur = data['article']['auteur'];
-      final DateTime createdAt = DateTime.parse(data['created_at']);
+      final DateTime createdAt = DateTime.parse(data['article']['created_at']);
       final String hrefAuteur = data['links']['articles_author']['href'];
-      print(id);
-      final Article fetchedArticle = Article(
+
+      fetchedArticle = Article(
         id: id,
-        titre: titre,
+        titre: titre, 
         resume: resumeArticle,
-        contenu: contentArticle,
-        auteur: auteur,
-        createdAt: createdAt,
-        href: widget.article.href,
-        hrefAuteur: hrefAuteur,
-      );
-      setState(() {
-        widget.article = fetchedArticle;
-      });
+        contenu: contenuArticle, 
+        auteur: auteur, 
+        createdAt: createdAt, 
+        href: widget.article.href, 
+        hrefAuteur: hrefAuteur);
+      print(fetchedArticle);
+      setState(() {});
     } else {
       throw Exception('Failed to fetch article');
     }
@@ -60,40 +60,38 @@ class _ArticlePageState extends State<ArticlePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.article.titre),
+        title: Text(fetchedArticle?.titre ?? widget.article.titre),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Titre: ${widget.article.titre}'),
-            SizedBox(height: 16),
-            Text('Résumé: ${widget.article.resume}'),
-            SizedBox(height: 16),
-            Text('Contenu: ${widget.article.contenu}'),
-            SizedBox(height: 16),
+            Text('Titre: ${fetchedArticle?.titre ?? widget.article.titre}'),
+            const SizedBox(height: 16),
+            Text('contenu : ${fetchedArticle?.contenu ?? widget.article.contenu}'),
+            const SizedBox(height: 16),
             GestureDetector(
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => AuthorArticlesPage(
-                      article: widget.article,
+                      article: fetchedArticle ?? widget.article,
                     ),
                   ),
                 );
               },
               child: Text(
-                'Auteur: ${widget.article.auteur}',
-                style: TextStyle(
+                'Auteur: ${fetchedArticle?.auteur ?? widget.article.auteur}',
+                style: const TextStyle(
                   color: Colors.blue,
                   decoration: TextDecoration.underline,
                 ),
               ),
             ),
-            SizedBox(height: 16),
-            Text('Créé le: ${widget.article.createdAt.toString()}'),
+            const SizedBox(height: 16),
+            Text('Créé le: ${fetchedArticle?.createdAt.toString() ?? widget.article.createdAt.toString()}'),
           ],
         ),
       ),
